8813f02494d29eeca46973ec3134583df3a9c4ed
Yenifer-Mariana <yenifermarianaguauna@iemmmrip.edu.co>
2024-10-28 10:31:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function calcularIMC() {
+    
+    var alturaEnCentimetros = parseInt(document.getElementById('talla').value);
+    var alturaEnMetros = alturaEnCentimetros / 100;
+    var pesoEnKilos = parseInt(document.getElementById('peso').value);
+  
+    var imc = Math.round(pesoEnKilos / (alturaEnMetros * alturaEnMetros));
+
+    var clasificacion;
+
+    if (imc < 18.5) {
+      clasificacion = 'estás muy delgado';
+    } else if (imc < 25) {
+      clasificacion = 'estás saludable';
+    } else {
+      clasificacion = 'tienes sobrepeso';
+    }
+    
+    var respuesta = 'tu IMC es ' + imc + ' y tu ' + clasificacion;
+    alert(respuesta);
+  }
